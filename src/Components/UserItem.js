7f3b8cc77a2bd6326c12fbd534e470f817474277
Christopher Harvey <chharvey@icloud>
2019-10-14 23:15:40 -0400

    Navbar state

--- src/Components/UserItem.js
+++ src/Components/UserItem.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
+  constructor() {
+    super();
+    this.state = {
+      id: 'id',
+      login: 'Harvey783',
+      avatar_url: 'https://avatars1.githubusercontent.com/u/25391865?s=40&v=4',
+      html_url: 'https://github.com/Harvey783'
+    };
+  }
   render() {
-    return <div>UserItem</div>;
+    return (
+      <div className="card text-center">
+        <img src={this.state.avatar_url} alt="" style={{ width: '60px' }} />
+        <h4>{this.state.login}</h4>
+      </div>
+    );
   }
 }
 
