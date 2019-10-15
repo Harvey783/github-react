bba05fa65042f896308e6882fb163c610943f23a
Christopher Harvey <chharvey@icloud>
2019-10-14 22:47:40 -0400

    UserItem Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from './Components/Navbar';
+import UserItem from './Components/UserItem';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Navbar></Navbar>
+        <Navbar />
+        <UserItem />
       </div>
     );
   }
