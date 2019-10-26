ff4b3129c80ae58c639de001a97f7721d2c79bbc
Christopher Harvey <chharvey@icloud>
2019-10-26 14:05:43 -0400

    state refactor

--- src/Components/UserItem.js
+++ src/Components/UserItem.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 
 class UserItem extends Component {
-  constructor() {
-    super();
-    this.state = {
-      id: 'id',
-      login: 'Harvey783',
-      avatar_url: 'https://avatars1.githubusercontent.com/u/25391865?s=40&v=4',
-      html_url: 'https://github.com/Harvey783'
-    };
-  }
+  state = {
+    id: 'id',
+    login: 'Harvey783',
+    avatar_url: 'https://avatars1.githubusercontent.com/u/25391865?s=40&v=4',
+    html_url: 'https://github.com/Harvey783'
+  };
+
   render() {
     const { login, avatar_url, html_url } = this.state;
     return (
