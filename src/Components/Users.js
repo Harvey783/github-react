3e55d14951670cf850a2ed09fd72d31a4f6a0ce4
Christopher Harvey <chharvey@icloud>
2019-10-26 14:35:03 -0400

    userStyle grid

--- src/Components/Users.js
+++ src/Components/Users.js
@@ -30,7 +30,7 @@ class Users extends Component {
 
   render() {
     return (
-      <div className="">
+      <div style={userStyle} className="">
         {this.state.users.map(user => (
           <UserItem key={user.id} className="" user={user} />
         ))}
@@ -38,5 +38,11 @@ class Users extends Component {
     );
   }
 }
+
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gridGap: '1rem'
+};
 
 export default Users;
