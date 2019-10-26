61cf68b4512c8fd29a51e0ce4772e93abfccf464
Christopher Harvey <chharvey@icloud>
2019-10-26 14:48:26 -0400

    Navbar Functional Refactor

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar background-primary">
-        <h3>
-          <i className="fab fa-github"></i>
-          Github React
-        </h3>
-      </nav>
-    );
-  }
-}
+const Navbar = () => {
+  return (
+    <nav className="navbar background-primary">
+      <h3>
+        <i className="fab fa-github"></i>
+        Github React
+      </h3>
+    </nav>
+  );
+};
 
 export default Navbar;
