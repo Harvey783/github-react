3105e59b366d913ad4ee0e7ddf3512b91f20646d
Chris <chharvey@icloud.com>
2019-10-31 04:24:37 -0400

    Axios Promise

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import Navbar from './Components/Navbar';
 import Users from './Components/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users')
+      .then(response => console.log(response.data));
+  }
+
   render() {
     return (
       <div>
