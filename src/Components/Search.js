2e788c922863f38b1324b67ff10c53dba31989c1
Chris <chharvey@icloud.com>
2019-11-09 17:45:53 -0500

    Search onChange

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,11 +1,25 @@
 import React from 'react';
 
 class Search extends React.Component {
+  state = {
+    text: ''
+  };
+
+  onChange = event => {
+    this.setState({ text: event.target.value });
+  };
+
   render() {
     return (
       <div>
         <form className="">
-          <input type="text" name="text" placeholder="Search for Users" />
+          <input
+            type="text"
+            name="text"
+            placeholder="Search for Users"
+            value={this.state.text}
+            onChange={this.onChange}
+          />
           <input type="submit" value="Search" />
         </form>
       </div>
