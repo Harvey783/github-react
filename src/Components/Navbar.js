7c06ca99bbae21ca89e10bde5253127fb3bd475a
Chris <chharvey@icloud.com>
2019-11-27 00:57:22 -0500

    Navbar link

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar background-primary">
-      <h3>
-        <i className="fab fa-github"></i>
-        Github React
-      </h3>
+      <Link to="/">
+        <h3>
+          <i className="fab fa-github"></i>
+          Github React
+        </h3>
+      </Link>
+      <ul>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
     </nav>
   );
 };
