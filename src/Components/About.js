fa606413ec763320edc28cc11c5e28888342b37d
Chris <chharvey@icloud.com>
2019-11-26 23:58:34 -0500

    About

---
+++ src/Components/About.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h3>About</h3>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dignissimos
+        corporis unde sit iusto quibusdam officiis autem distinctio harum
+        laboriosam. Reprehenderit repellendus quia recusandae tenetur voluptas
+        dolores quod, optio, ab veritatis omnis voluptatibus sint doloremque,
+        saepe assumenda iure natus beatae repudiandae velit sed sit consequuntur
+        modi? Totam repellendus iusto odit nemo!
+      </p>
+    </Fragment>
+  );
+};
+
+export default About;
